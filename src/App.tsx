b6255fa01e4eea2cb6cfa4272e74c86a7da6c354
Matheus Santos <matheus.pires@sp.senai.br>
2021-10-20 11:50:08 -0300

    feat: add SendMessage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { LoginBox, MessageList } from "./components";
 
+import { useAuth } from "./contexts/useAuth";
+
 import styles from "./App.module.scss";
+import { SendMessageForm } from "./components/SendMessageForm";
 
 export function App() {
+  const { user } = useAuth();
+
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
-      <LoginBox />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
   );
 }
